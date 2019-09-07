10996a703e78d22a78b092434ef549a028fd5e22
Aaron Jackson <aarontjackson@outlook.com>
2019-09-07 15:31:54 -0400

    outline of html for questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
 //identify global variables
-var = question 
+var triviaQuestion;
+var answer;
+var rightAnswers = 0;
+var wrongAnswers = 0;
+var questionTimer;
+
+
+
+//function that can diplay to the user question and associated answers
+function triviaQuestion(question, a1, a2, a3, a4, t, f) {
+    this.quesition = question;
+    this.a1 = a1; 
+    this.a1 = a1;
+    this.a1 = a1;
+    this.a1 = a1;
+    this.t = t;
+    this.f = f;
+}
+
+
+
 
 //create 10 questions for the user to answer (true/false & multiple choice)/function to track timer
 
