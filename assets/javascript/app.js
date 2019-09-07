f4a14c0ab72b46add1933cea8d6b45099da0ab15
Aaron Jackson <aarontjackson@outlook.com>
2019-09-07 14:47:11 -0400

    linked jquery, css styelsheet, and javascipt to index.html page

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//identify global variables
+var = question 
+
+//create 10 questions for the user to answer (true/false & multiple choice)/function to track timer
+
+//create statements for correct and incorrect answers (ideally using if/else)/add a timer function
+
+//display total number of correct and incorrect answers with restart prompt
+
+});
