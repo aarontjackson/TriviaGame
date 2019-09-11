d0f1d6bc11383bff9b98f4af139ac2615a0940b3
Aaron Jackson <aarontjackson@outlook.com>
2019-09-10 22:39:23 -0400

    on click function for wins/losses & reset button with Bootstrap functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,201 +1,208 @@
-$(document).ready(function () {
-    //identify global variables
-    var rightAnswers = 0;
-    var wrongAnswers = 0;
-    var questionTimer = 30;
-    var questionCount = 0;
-    var clock;
-    var userPick;
-    var i = [];
-    var index = [];
+//create 10 questions for the user to answer (true/false or multiple choice)
+var triviaQuestions = [
+    {
+        question: "Forthy degrees fahrenheit is the same as _______ celsius.",
+        choices: ["Forthy Degrees Celsius", "Twenty Degrees Celsius", "Thirty Degress Celsius", "Fifty Degress Celsius"],
+        answer: "Forthy Degrees Celsius"
+    },
+
+    {
+        question: "How many letters are in the Hawaiian alphabet?",
+        choices: ["27", "26", "13", "23"],
+        answer: "13"
+    },
+
+    {
+        question: "The term OMG was first used in?",
+        choices: ["1994", "1917", "1984", "2001"],
+        answer: "1917"
+    },
+
+    {
+        question: "Two full grown trees can support a family of?",
+        choices: ["4", "2", "3", "5"],
+        answer: "4"
+    },
+
+    {
+        question: "A spider's silk is how many times stronger than steel?",
+        choices: ["4", "2", "3", "5"],
+        answer: "3"
+    },
+
+    {
+        question: "The sun makes up _______ % of the mass in our Solar System.",
+        choices: ["85", "20", "99", "90"],
+        answer: "99"
+    },
+
+    {
+        question: "Lettuce is to sunflower seeds, as a peach is to _____ ",
+        choices: ["Almonds", "Soybeans", "Onions", "Cabbage"],
+        answer: "Almonds"
+    },
+
+    {
+        question: "How many rings make up the symbol of the Olympic Games",
+        choices: ["Three", "Five", "Six", "Seven"],
+        answer: "Five"
+    },
+
+    {
+        question: "As the old idiom goes practice makes _______",
+        choices: ["Failure", "Performance", "Pancakes", "Perfect"],
+        answer: "Perfect"
+    },
+
+    {
+        question: "What is the most commonly eaten food in the world?",
+        choices: ["Wheat", "Potatoes", "Corn", "Beans"],
+        answer: "Corn"
+    }
+];
+
+//identify global variables
+var counter = 30;
+var currentQuestion = 0;
+var score = 0;
+var lost = 0;
+var timer;
+
+function nextQuestion() {
+
+    //Continue through the trivia question array until all questions are displayed
+    var questionComplete = (triviaQuestions.length - 1) === currentQuestion;
+
+    if (questionComplete) {
+        console.log("Done!");
+        displayResults();
+    }
+    else {
+        currentQuestion++;
+        loadQuestion();
+    }
+
+}
+
+// Timer functionality 
+
+// Change the question if the timer reaches zero
+function timeUp() {
+    clearInterval(timer);
+
+    lost++;
+
+    nextQuestion();
+}
+// Timer countdown function
+function countDown() {
+    counter--;
+
+    $("#time").html("Time Remaining: " + counter);
+
+    if (counter === 0) {
+        timeUp();
+    }
+}
+// Load the question and answers with a 30 second timer for user to answer
+function loadQuestion() {
+    counter = 30;
+    timer = setInterval(countDown, 1000);
+
+    var question = triviaQuestions[currentQuestion].question; //
+    var choices = triviaQuestions[currentQuestion].choices; //
+
+    $("#time").html("Time Remaining: " + counter);
+    $("#game").html(`  
+        <h4>${question}</h4>
+        ${loadChoices(choices)}
+        `);
+
+}
+
+// Timer for user to select an answer
 
 
-    //create 10 questions for the user to answer (true/false or multiple choice)
 
-    var triviaQuestions = [
-        {
-            question: "Forthy degrees fahrenheit is the same as _______ celsius.",
-            choices: ["Forthy Degrees Celsius", "Twenty Degrees Celsius", "Thirty Degress Celsius", "Fifty Degress Celsius"],
-            answer: "Forthy Degrees Celsius",
-            image: ""
-        },
+// Function to show all answer choices to the user
+function loadChoices(choices) {
 
-        {
-            question: "How many letters are in the Hawaiian alphabet?",
-            choices: ["27", "26", "13", "23"],
-            answer: "13",
-            image: ""
-        },
+    var result = "";
+    // Iterate through the array to show all possible choices
+    for (let i = 0; i < choices.length; i++) {
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`
 
-        {
-            question: "The term OMG was first used in?",
-            choices: ["1994", "1917", "1984", "2001"],
-            answer: "1917",
-            image: ""
-        },
+    }
 
-        {
-            question: "Two full grown trees can support a family of?",
-            choices: ["4", "2", "3", "5"],
-            answer: "4",
-            image: ""
-        },
+    return result;
+}
+loadQuestion();
 
-        {
-            question: "A spider's silk is how many times stronger than steel?",
-            choices: ["4", "2", "3", "5"],
-            answer: "3",
-            image: ""
-        },
+// Event delegation for user selecting right/wrong answer
+$(document).on("click", ".choice", function () {
+    clearInterval;
+    var selectedAnswer = $(this).attr("data-answer");
+    var answer = triviaQuestions[currentQuestion].answer;
+    console.log("click is up", selectedAnswer);
 
-        {
-            question: "The sun makes up _______ % of the mass in our Solar System.",
-            choices: ["85", "20", "99", "90"],
-            answer: "99",
-            image: ""
-        },
+    if (answer === selectedAnswer) {
+        score++;
+        nextQuestion();
+    }
+    else {
+        lost++;
+        nextQuestion();
+    }
+});
 
-        {
-            question: "Lettuce is to sunflower seeds, as a peach is to _____ ",
-            choices: ["Almonds", "Soybeans", "Onions", "Cabbage"],
-            answer: "Almonds",
-            image: ""
-        },
+function displayResults() {
+    var result = `
+            <p> You answered ${score} question(s) right!</p>
+            <p> You missed ${lost} question(s)!</p>
+            <p> A total of ${triviaQuestions.length} question(s) right!</p>
+            <button class="btn btn-primary" id="reset">Reset Game</button>
+        `;
 
-        {
-            question: "How many rings make up the symbol of the Olympic Games",
-            choices: ["Three", "Five", "Six", "Seven"],
-            answer: "Five",
-            image: ""
-        },
+    $("#game").html(result);
+}
 
-        {
-            question: "As the old idiom goes practice makes _______",
-            choices: ["Failure", "Performance", "Pancakes", "Perfect"],
-            answer: "Perfect",
-            image: ""
-        },
+$(document).on("click", "#reset", function () {
+    counter = 30;
+    currentQuestion = 0;
+    score = 0;
+    lost = 0;
+    timer;
 
-        {
-            question: "What is the most commonly eaten food in the world?",
-            choices: ["Wheat", "Potatoes", "Corn", "Beans"],
-            answer: "Corn",
-            image: ""
-        }
+    loadQuestion();
+});
 
-    ];
+loadQuestion();
 
-
-    //Timer to answer questions
-
-    // Button to start the timer
-    $("#startBtn").click(function() {
-        $("#startBtn").hide();
-        displayQuestion();
-        startTrivia();
-        for (let index = 0; index < triviaQuestion.length; index++) {
-            $("#question").text(triviaQuestion[index]);
-        }
-    });
-
-
-    // Button to reset the game
-    $("#resetBtn").click(stopTrivia);
-
-  
-
-    // Start the trivia game timer
-    function startTrivia() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
-    };
-
-    function decrement() {
-
-        questionTimer--;
-
-        $("#timer").html("<span>" + questionTimer + "</span>");
-
-        if (questionTimer === 0) {
-
-            stopTrivia();
-
-        };
-    };
-
-    // Stop the tirivia game  
-    function stopTrivia() {
-        // ClearInterval for the question:
-        clearInterval(intervalId);
-    };
-
-    startTrivia();
-
-
-    function displayQuestion(display) {
-        
-    
-        randomQuestion = Math.floor(Math.random() * triviaQuestion.length);
-
-        display = (triviaQuestion[index]);
-
-        console.log(display);
-        
-        // Display question onto the window
-        $("#question").html("<div>" + display.question + "</div>");
-
-        // Display all answer choices for user selection
-        for (let i = 0; i < display.choices.length; i++) 
-            $("#answer").html ("<br><div>" + display.choices + "</div><br>");
-        // Record user selections into an array
-            userPick.attr("userValue", i);
-            $("#answer").append(userPick);
-
-    };
-
-    function changeQuestion() {
-        count++;
-
-        // Display question onto the window
-        $("#question").html("<div>" + display.question + "</div>");
-
-        // Display all answer choices for user selection
-        for (let i = 0; i < display.choices.length; i++) {
-            $("#answer").html ("<br><div>" + display.choices + "</div><br>");
-        // Record user selections into an array
-            userPick.attr("userValue", i);
-            $("#answer").append(userPick);
-        };
-    };
-
-    // Event delegation for user selecting an answer
-    $("#answer").click(function() {
 
         // Document user array position to compare to integer value of answer
-        userPick = parseInt.attr("userValue");
+        // userPick = parseInt.attr("userValue");
 
         // Trivia game outcomes
 
-        if (userPick === display.answer ) {
-            stopTrivia();
-            rightAnswers++;
-            $("#message").html("<p>Correct!<p>");
-        }
+    //     if (userPick === display.answer ) {
+    //         stopTrivia();
+    //         rightAnswers++;
+    //         $("#message").html("<p>Correct!<p>");
+    //     }
 
-        else {
-            stopTrivia();
-            wrongAnswers++;
-            $("#message").html("<p>Wrong! The correct answer is: " + display.choices[display.answer] + "<p>");
-        };
-    });
+    //     else {
+    //         stopTrivia();
+    //         wrongAnswers++;
+    //         $("#message").html("<p>Wrong! The correct answer is: " + display.choices[display.answer] + "<p>");
+
+    // });
 
     // Trivia reset
-    $("#resetBtn").click(function() {
-     
+    // $("#resetBtn").click(function() {
 
-    });
 
-});
+    // });
+
 
 
 
@@ -221,4 +228,3 @@ $(document).ready(function () {
 
         //function to restart the game
 
-   
