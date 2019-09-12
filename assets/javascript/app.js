8264d1eb484d65c154932c1ec1cab963a0444ad0
Aaron Jackson <aarontjackson@outlook.com>
2019-09-11 20:49:00 -0400

    upadted images for right/wrong answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,89 +3,110 @@ var triviaQuestions = [
     {
         question: "Forthy degrees fahrenheit is the same as _______ celsius.",
         choices: ["Forthy Degrees Celsius", "Twenty Degrees Celsius", "Thirty Degress Celsius", "Fifty Degress Celsius"],
-        answer: "Forthy Degrees Celsius"
+        answer: "Forthy Degrees Celsius",
+        rightImage: "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "How many letters are in the Hawaiian alphabet?",
         choices: ["27", "26", "13", "23"],
-        answer: "13"
+        answer: "13",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "The term OMG was first used in?",
         choices: ["1994", "1917", "1984", "2001"],
-        answer: "1917"
+        answer: "1917",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "Two full grown trees can support a family of?",
         choices: ["4", "2", "3", "5"],
-        answer: "4"
+        answer: "4",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "A spider's silk is how many times stronger than steel?",
         choices: ["4", "2", "3", "5"],
-        answer: "3"
+        answer: "3",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "The sun makes up _______ % of the mass in our Solar System.",
         choices: ["85", "20", "99", "90"],
-        answer: "99"
+        answer: "99",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "Lettuce is to sunflower seeds, as a peach is to _____ ",
         choices: ["Almonds", "Soybeans", "Onions", "Cabbage"],
-        answer: "Almonds"
+        answer: "Almonds",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "How many rings make up the symbol of the Olympic Games",
         choices: ["Three", "Five", "Six", "Seven"],
-        answer: "Five"
+        answer: "Five",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "As the old idiom goes practice makes _______",
         choices: ["Failure", "Performance", "Pancakes", "Perfect"],
-        answer: "Perfect"
+        answer: "Perfect",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     },
 
     {
         question: "What is the most commonly eaten food in the world?",
         choices: ["Wheat", "Potatoes", "Corn", "Beans"],
-        answer: "Corn"
+        answer: "Corn",
+        rightImage:  "./assets/images/thermoRight.gif",
+        wrongImage: "./assets/images/thermoWrong.gif"
     }
 ];
 
-var rightImages = [
-    "./assets/images/thermoRight.gif",
-    "./assets/images/alphabetRight.gif",
-    "./assets/images/omgRight.gif",
-    "./assets/images/treesRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif"
-];
+//Images for right and wrong user selections
+// var rightImages = [
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/alphabetRight.gif",
+//     "./assets/images/omgRight.gif",
+//     "./assets/images/treesRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif"
+// ];
 
-var wrongImages = [
-    "./assets/images/thermoWrong.gif",
-    "./assets/images/alphabetWrong.gif",
-    "./assets/images/omgWrong.gif",
-    "./assets/images/treesWrong.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif",
-    "./assets/images/thermoRight.gif"
-];
+// var wrongImages = [
+//     "./assets/images/thermoWrong.gif",
+//     "./assets/images/alphabetWrong.gif",
+//     "./assets/images/omgWrong.gif",
+//     "./assets/images/treesWrong.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif",
+//     "./assets/images/thermoRight.gif"
+// ];
 
 //identify global variables
 var counter = 30;
@@ -94,27 +115,26 @@ var score = 0;
 var lost = 0;
 var timer;
 
+//Continue through the trivia question array until all questions are displayed
 function nextQuestion() {
-
-    //Continue through the trivia question array until all questions are displayed
     var questionComplete = (triviaQuestions.length - 1) === currentQuestion;
 
     if (questionComplete) {
-        console.log("Done!");
+        console.log("Game is done!");
         displayResults();
     }
     else {
+        console.log("Continue on!");
         currentQuestion++;
         loadQuestion();
     }
 
 }
 
-// Timer functionality 
+// 30 second timer functionality 
 
 // Change the question if the timer reaches zero
 function timeUp() {
-
     clearInterval(timer);
 
     lost++;
@@ -126,7 +146,6 @@ function timeUp() {
 
 // Timer countdown function
 function countDown() {
-
     counter--;
 
     $("#time").html("Time Remaining: " + counter);
@@ -138,43 +157,35 @@ function countDown() {
 
 // Load the question and answers with a 30 second timer for user to answer
 function loadQuestion() {
-
     counter = 30;
+
     timer = setInterval(countDown, 1000);
 
-    var question = triviaQuestions[currentQuestion].question; //
-    var choices = triviaQuestions[currentQuestion].choices; //
+    var question = triviaQuestions[currentQuestion].question;
+    var choices = triviaQuestions[currentQuestion].choices;
 
     $("#time").html("Time Remaining: " + counter);
     $("#game").html(`  
         <h4>${question}</h4>
         ${loadChoices(choices)}
-        `);
+    `);
 }
-
-// Timer for user to select an answer
-
-
 
 // Function to show all answer choices to the user
 function loadChoices(choices) {
-
     var result = "";
     // Iterate through the array to show all possible choices
     for (let i = 0; i < choices.length; i++) {
-        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
 
     }
 
     return result;
 }
 
-loadQuestion();
-
 // Event delegation for user selecting right/wrong answer
 $(document).on("click", ".choice", function () {
-
-    clearInterval;
+    clearInterval(timer);
 
     var selectedAnswer = $(this).attr("data-answer");
 
@@ -199,9 +210,9 @@ $(document).on("click", ".choice", function () {
 function displayResults() {
 
     var result = `
-            <p> You answered ${score} question(s) right!</p>
-            <p> You missed ${lost} question(s)!</p>
-            <p> A total of ${triviaQuestions.length} question(s) right!</p>
+            <p>You answered ${score} question(s) right!</p>
+            <p>You missed ${lost} question(s)!</p>
+            <p>A total of ${triviaQuestions.length} question(s) right!</p>
             <button class="btn btn-primary" id="reset">Reset Game</button>
         `;
 
@@ -209,7 +220,6 @@ function displayResults() {
 }
 
 $(document).on("click", "#reset", function () {
-
     counter = 30;
     currentQuestion = 0;
     score = 0;
@@ -219,99 +229,33 @@ $(document).on("click", "#reset", function () {
     loadQuestion();
 });
 
-// Check to see if there are any remaining unanswered questions
-// function remainingQuestions() {
-//     var questionsLeft = triviaQuestions.length - (currentQuestion +1);
-//     var totalQuestion = triviaQuestions.length;
-
-//     return remainingQuestions:
-// }
-
-//Image array for right or wrong selections
-
-function triviaImages(images) {
-
-    var showImage = images[i];
-    return triviaImages;
-
-}
-
 // Display images for right or wrong questions
-
 function loadImages(record) {
-    
-    var correctAnswer = triviaQuestions[currentQuestion].correctAnswer;
+    var answer = triviaQuestions[currentQuestion].answer;
+    var rightImage = triviaQuestions[currentQuestion].rightImage;
+    var wrongImage = triviaQuestions[currentQuestion].wrongImage;
 
     if (record === "win") {
-        $("#games").html(`
-            <p class="preload-image">The correct</p>
-            <p class="preload-image">The correct answer is <strong>${correctAnswer}</strong></p>
-            <img src="${triviaImages(rightImages)}"/>
+        $("#game").html(`
+            <p class="preload-image">Correct! You know your random facts!</p>
+            <p class="preload-image">The correct answer is <strong>${answer}</strong></p>
+            <img src="${rightImage}"/>
         `);
     }
     else {
-        $("#games").html(`
-            <p class="preload-image">Wrong! The correct answer is <strong>${correctAnswer}</strong></p>
+        $("#game").html(`
+            <p class="preload-image">Wrong! The correct answer is <strong>${answer}</strong></p>
             <p class="preload-image"> So close... but wrong</p>
-            <img src="${triviaImages(wrongImages)}"/>
+            <img src="${wrongImage}"/>
         `);
     }
 
 }
 
-$("#start").click(funtion() {
+$("#start").click(function() {
     $("#start").hide();
     $("#time").html(counter);
     loadQuestion();
 });
 
-
-        // Document user array position to compare to integer value of answer
-        // userPick = parseInt.attr("userValue");
-
-        // Trivia game outcomes
-
-    //     if (userPick === display.answer ) {
-    //         stopTrivia();
-    //         rightAnswers++;
-    //         $("#message").html("<p>Correct!<p>");
-    //     }
-
-    //     else {
-    //         stopTrivia();
-    //         wrongAnswers++;
-    //         $("#message").html("<p>Wrong! The correct answer is: " + display.choices[display.answer] + "<p>");
-
-    // });
-
-    // Trivia reset
-    // $("#resetBtn").click(function() {
-
-
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //create statements for correct and incorrect answers (ideally using if/else)/add a timer function
-
-        //display total number of correct and incorrect answers with 
-
-        //function to restart the game
 
